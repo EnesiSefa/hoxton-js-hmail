4aae1565cca2c3c32cec5ad318d76f0b7e323fee
enesi sefa <enesisefa380@gmail.com>
2022-06-29 17:06:29 +0200

    added single email list

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
-
-
-
-
-
 import './style.css'
 
 
 type State ={
-  emails : Email[]
+  emails : Array<Email> // or Email[]
 }
 
 type Email = {
@@ -56,13 +51,15 @@ const state : State = {
 }
 
 function render(){
-
+  renderEmails()
+  
 }
-renderEmails()
+render()
 
 
 function renderEmails(){
-let ulEl = document.createElement("ul")
+
+  let ulEl = document.createElement("ul")
   ulEl.className = "emails-list"
 let h1El = document.createElement("h1")
 h1El.textContent = "Inbox"
@@ -73,6 +70,10 @@ for(let item of state.emails){
 
   let liEl = document.createElement("li")
   liEl.className = "emails-list__item"
+  liEl.addEventListener("click" , ()=>{
+    renderSingleEmail()
+    render
+  })
 
   let spanEl = document.createElement("span")
   spanEl.className="emails-list__item__read-icon material-symbols-outlined"
@@ -95,39 +96,53 @@ for(let item of state.emails){
    ulEl.append(liEl)
    liEl.append(spanEl,imgEl,pEl,pEl2)
 }}
-// function renderEmail(item : Email){
- 
- 
-//   let h1El = document.createElement("h1")
-//   h1El.textContent = "Inbox"
 
-//   let ulEl = document.createElement("ul")
-//   ulEl.className = "emails-list"
-//   let liEl = document.createElement("li")
-//   liEl.className = "emails-list__item"
 
-//   let spanEl = document.createElement("span")
-//   spanEl.className="emails-list__item__read-icon material-symbols-outlined"
-//   spanEl.textContent = "mark_email_unread"
-//    let imgEl = document.createElement("img")
-//    imgEl.className = "emails-list__item__image"
-//    imgEl.src = item.img
 
-//    let pEl = document.createElement("p")
-//    pEl.className = "emails-list__item__from"
-//    pEl.textContent = "Nico"
 
-//    let pEl2  = document.createElement("p")
-//    pEl2.className = "emails-list__item__content"
-//    pEl2.textContent = "Link to today's video and slides is up!"
 
-//    let mainEl = document.querySelector("main")
-//    mainEl.append(h1El,ulEl)
-//    ulEl.append(liEl)
-//    liEl.append(spanEl,imgEl,pEl,pEl2)
+function renderSingleEmail(){
+ for(let item of state.emails) {
+  let sectionEl = document.createElement("section")
+  sectionEl.className = "single-email"
 
+  let btnEl = document.createElement("button")
+  btnEl.className = "single-email__button"
+  btnEl.textContent = "⬅Back"
+
+  let divEl = document.createElement("div")
+  divEl.className = "single-email__sender-section"
    
-// }
+  let imgEl = document.createElement("img")
+  imgEl.className = "single-email__image"
+  imgEl.src = item.img
+
+  let spanEl = document.createElement("span")
+  spanEl.className = "single-email__sender"
+  spanEl.textContent = item.emailAddress
+
+  let h1El = document.createElement("h1")
+  h1El.className = "single-email__header"
+  h1El.textContent = item.header
+
+  let pEl = document.createElement("p")
+  pEl.className = "single-email__content"
+  pEl.textContent= item.content
+
+
+  let mainEl = document.querySelector("main")
+  mainEl.append(sectionEl)
+  sectionEl.append(btnEl,divEl,h1El,pEl)
+  divEl.append(imgEl,spanEl)
+}
+
+}
+
+
+
+
+
+
 // Instructions
 // Using the provided emails and template files:
 // - Create all the state data you need to make your app work
